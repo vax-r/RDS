26de04c1aff3d03c82ec1aa4bdcfa836156135e9
I Hsin Cheng <richard120310@gmail.com>
2024-09-10 10:57:16 +0800

    list: Implementation of unit test for list_replace()
    Signed-off-by: I Hsin Cheng <richard120310@gmail.com>

--- src/list.rs
+++ src/list.rs
@@ -384,4 +384,23 @@ mod tests {
         assert!(Rc::ptr_eq(b.borrow().prev.as_ref().unwrap(), &list));
         assert!(ListHead::list_empty(a.clone()));
     }
+
+
+    #[test]
+    fn test_list_replace() {
+        let a_old = ListHead::new(0);
+        let a_new = ListHead::new(0);
+        let b = ListHead::new(0);
+        let list = ListHead::new(0);
+
+        ListHead::list_add_tail(a_old.clone(), list.clone());
+        ListHead::list_add_tail(b.clone(), list.clone());
+
+        ListHead::list_replace(&a_old, &a_new);
+
+        assert!(Rc::ptr_eq(list.borrow().next.as_ref().unwrap(), &a_new));
+        assert!(Rc::ptr_eq(b.borrow().prev.as_ref().unwrap(), &a_new));
+        assert!(Rc::ptr_eq(a_new.borrow().next.as_ref().unwrap(), &b));
+        assert!(Rc::ptr_eq(a_new.borrow().prev.as_ref().unwrap(), &list));
+    }
 }
